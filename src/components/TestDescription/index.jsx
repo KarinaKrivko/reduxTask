b9a3f1a25d5a4ae32ecf4fcee615a475410e0300
KarinaK <mskarinakrivko@gmail.com>
2023-04-27 10:30:55 +0200

    test component added

---
+++ src/components/TestDescription/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TestDescription(props) {
+    return (
+        <div>
+            Test from Karina
+        </div>
+    );
+}
+
+export default TestDescription;
